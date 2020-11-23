7e7e1e7bf37d4b66d659f418f95909873f4f61b6
emys-alb <emilly.oliveira@ccc.ufcg.edu.br>
2020-11-22 21:21:02 -0300

    Create react app

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>O Py tá On</h1>
+       <h2>Aplicação para auxiliar monitores e alunos a se comunicarem</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
